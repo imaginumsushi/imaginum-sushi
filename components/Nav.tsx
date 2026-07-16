3643f73cd8173e0dc43b068ab1b57cd9e0d0c8f7
imaginumsushi <imaginumsushi@gmail.com>
2026-07-16 14:51:43 -0300

    Update: add real logo to nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Nav() {
   return (
     <nav className="nav">
-      <Link href="/" className="nav-logo">
-        IMAGINUM <span>SUSHI</span>
+      <Link href="/">
+        <Image src="/logo.png" alt="Imaginum Sushi" width={140} height={42} style={{objectFit:'contain'}} priority />
       </Link>
       <div className="nav-links">
         <Link href="/cardapio">Cardápio</Link>
